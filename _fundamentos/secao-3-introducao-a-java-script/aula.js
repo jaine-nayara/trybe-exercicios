a5475019f62cd9761bf37de13ed52270e0aa7f80
Jaine <117602018+jaine-nayara@users.noreply.github.com>
2023-01-24 13:21:53 -0300

    secao3 pelo git

---
+++ _fundamentos/secao-3-introducao-a-java-script/aula.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 10;
+let multiplicacao;
+if (num1==num2){
+    console.log(num1 + num2);
+} else {
+    multiplicacao = num1 * num2;
+    console.log(multiplicacao);
+}
---
+++ _fundamentos/secao-3-introducao-a-java-script/variaveis.js
@@ -0,0 +1,4 @@
+const myName = "Jaine";
+const birthCity = "Pindamonhangaba";
+let birthYear = 1997;
+console.log(myName, birthCity, birthYear)
