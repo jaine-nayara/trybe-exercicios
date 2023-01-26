7fc00fd6e3ce893d084d3325e78e118c8f49551d
Jaine Oliveira <jaine.nayara.23@gmail.com>
2023-01-26 19:26:16 -0300

    finalizado lista de exercicios osbre objetos

--- _fundamentos/secao-3-introducao-a-java-script/objeto.js
+++ _fundamentos/secao-3-introducao-a-java-script/objeto.js
@@ -18,5 +18,79 @@ let car = {
 for (let index in car){
     console.log(index, car[index]);
 }
+//
+console.log();
+//exercícios do dia
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  //Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+  console.log("Bem-vinda, " + info.personagem)
+
+  //Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. 
+
+  info['recorrente'] = 'Sim',
+  console.log(info);
+
+  console.log();
+//Faça um for/in que mostre todas as chaves do objeto.
+for (let index in info){
+    console.log(index);
+}
 
 
+//Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+console.log();
+for (let index in info){
+    console.log(info[index]);
+}
+
+console.log();
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. 
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+
+for (let index in info){
+    if (info[index] == 'Sim' && info2[index] == 'Sim'){
+        console.log("Ambos recorrentes");
+    }else{
+    console.log(info[index] + " e " + info2[index])
+}
+}
+
+//LEITURA DE OBJETOS
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  //Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos[0].titulo + "'.");
+
+  //Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
+  leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  })
+  console.log(leitor.livrosFavoritos);
+
+  //Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
+  console.log();
+  console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos.")
