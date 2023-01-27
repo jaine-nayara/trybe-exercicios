bd6761c1c9ff914e064ab7aebe0dbbbd4ede8be4
Jaine Oliveira <jaine.nayara.23@gmail.com>
2023-01-27 19:16:48 -0300

    parte pratica do dia 5

---
+++ _fundamentos/secao-3-introducao-a-java-script/dia-5-ES6/exercicios.js
@@ -0,0 +1,33 @@
+// A sintaxe básica do operador ternário é muito simples:
+//`expressão verdadeira ou falsa` ? `retorno se verdadeira` : `retorno se falsa`;
+
+// Assim, por exemplo, podemos ter expressões com a seguinte estrutura:
+const trueExpression = (1 + 1 === 2) ? `isso é verdade` : `isso é mentira`;
+console.log(trueExpression); // isso é verdade
+
+const falseExpression = (2 + 2 === 3) ? `isso é verdade` : `isso é mentira`;
+console.log(falseExpression); // isso é mentira
+
+
+
+//Modifique as concatenações para template literals.
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá, ${name} ${lastName}!`)
+
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`)
+
+//Modifique a estrutura das funções a seguir para que elas sejam arrow functions;
+const numeroAleatorio = () => Math.random();
+console.log(numeroAleatorio());
+
+//Transforme a função hello em uma arrow function;
+const hello = nome =>`Olá, ${nome}!`;
+let nome = 'Ivan';
+console.log(hello(nome));
