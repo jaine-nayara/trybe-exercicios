cd59ab50bf0109965f0d58153dfdf0809c368c15
Jaine Oliveira <jaine.nayara.23@gmail.com>
2023-02-01 15:32:45 -0300

    corrigindo seção 4

--- _fundamentos/secao-3-introducao-a-java-script/dia-5-ES6/exercicios.js
+++ _fundamentos/secao-3-introducao-a-java-script/dia-5-ES6/exercicios.js
@@ -31,3 +31,8 @@ console.log(numeroAleatorio());
 const hello = nome =>`Olá, ${nome}!`;
 let nome = 'Ivan';
 console.log(hello(nome));
+
+//Altere a expressão if/else utilizando ternary operator;
+let speed = 90;
+const speedCar = (speed) => (speed >= 120) ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
+console.log(speedCar(speed));
