dfb5dcb84429822ed3f107f9014b5570df47eb63
Jaine Oliveira <jaine.nayara.23@gmail.com>
2023-02-17 11:54:55 -0300

    parte 2 e inicio parte 3

---
+++ _fundamentos/secao-6-teste-unitario/dia-1-fluxo-de-excecoes/parte3.js
@@ -0,0 +1,33 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+/* Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
+function trocaTurno(objeto, chave, valor) {
+  objeto[chave] = valor;
+  return objeto;
+}
+console.log(trocaTurno(lesson1,'turno', 'noite'));
+
+/* Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. */
+function listaKey(objeto) {
+  const chaves = Object.keys(objeto);
+  return chaves;
+}
+console.log(listaKey(lesson1))
