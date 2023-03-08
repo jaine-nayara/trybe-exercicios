30ef02937ad71cfc012b3cfb4ba5716be279342d
Jaine Oliveira <jaine.nayara.23@gmail.com>
2023-03-08 16:03:19 -0300

    dia 3 secao 7

---
+++ _fundamentos/secao-7-HOF/dia-3/dia3.js
@@ -0,0 +1,112 @@
+// Fonte: <https://restcountries.com/v2/all>
+const countries = [
+  {
+    name: "Afghanistan",
+    region: "Asia",
+    currencies: [{ code: "AFN", name: "Afghan afghani" }],
+    capital: "Kabul",
+    population: 40218234,
+    area: 652230,
+  },
+  {
+    name: "Åland Islands",
+    region: "Europe",
+    currencies: [{ code: "EUR", name: "Euro" }],
+    capital: "Mariehamn",
+    population: 28875,
+    area: 1580,
+  },
+  {
+    name: "Albania",
+    region: "Europe",
+    currencies: [{ code: "ALL", name: "Albanian lek" }],
+    capital: "Tirana",
+    population: 2837743,
+    area: 28748,
+  },
+  {
+    name: "Algeria",
+    region: "Africa",
+    currencies: [{ code: "DZD", name: "Algerian dinar" }],
+    capital: "Algiers",
+    population: 44700000,
+    area: 2381741,
+  },
+  {
+    name: "American Samoa",
+    region: "Oceania",
+    currencies: [{ code: "USD", name: "United States Dollar" }],
+    capital: "Pago Pago",
+    population: 55197,
+    area: 199,
+  },
+  {
+    name: "Andorra",
+    region: "Europe",
+    currencies: [{ code: "EUR", name: "Euro" }],
+    capital: "Andorra la Vella",
+    population: 77265,
+    area: 468,
+  },
+  {
+    name: "Angola",
+    region: "Africa",
+    currencies: [{ code: "AOA", name: "Angolan kwanza" }],
+    capital: "Luanda",
+    population: 32866268,
+    area: 1246700,
+  },
+  {
+    name: "Anguilla",
+    region: "Americas",
+    currencies: [{ code: "XCD", name: "East Caribbean dollar" }],
+    capital: "The Valley",
+    population: 13452,
+    area: 91,
+  },
+];
+
+/*  - Calcule a quantidade total da população de todos os países. */
+const getPopulation = () => {
+  countries.reduce((acc, curl) => acc + curl.population, 0);
+};
+
+/* Calcule a área total de todos os países. */
+const getTotalArea = () => {
+  return countries.reduce((acc, curl) => acc + curl.area, 0);
+};
+
+const longestName = () => {
+  return countries.reduce((acc, curl) => acc.name.length > curl.name.length ? acc : curl);
+};
+console.log(longestName());
+
+
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+const letras = names.join('').toLowerCase().split(''); //juntar o array em uma unica variavel, passar tudo para minuscula e separar letra por letra
+const countA = () => {
+  return letras.reduce((acc, curr) => curr === 'a' ? acc += 1 : acc, 0)
+}
+console.log(countA());
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const expectedResult = [
+ { name: 'Pedro Henrique', average: 7.8 },
+ { name: 'Miguel', average: 9.2 },
+ { name: 'Maria Clara', average: 8.8 },
+];
+const studentAverage = () => {
+  return students.map((student, index) =>
+  ({name: student,
+    average: (grades[index].reduce((acc, curr) => (acc + curr), 0)/5),
+  })
+)};
+
+console.log(studentAverage())
